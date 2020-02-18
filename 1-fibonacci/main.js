2b7fef8bec22c67bae2789a2a53318696f294f87
Unknown <perryx@gmail.com>
2020-02-18 11:52:58 -0700

    bigint and validation improvements

--- 1-fibonacci/main.js
+++ 1-fibonacci/main.js
@@ -32,26 +32,30 @@ const formatResult = (place, result) => {
   return `The <strong>${place}${indicator}</strong> number of the Fibonacci sequence is <strong>${result}</strong>.`;
 }
 
-const invalidInput = (input) => {
+const validateInput = (input) => {
+  if (input == "") {return `Hey, buddy, you wanna try entering an actual value?`}
   const parsed = parseInt(input)
   const vowels = ['a', 'e', 'i', 'o', 'u']
   const aOrAn = vowels.includes(input[0]) ? 'an' : 'a'
+  const invalidInput = input == '0' ? 'zero' : input
   if (input == 0 || isNaN(parsed)) {
-    return `What the fuck is ${aOrAn} \"${input == '0' ? 'zero' : input}\"th place?`;
+    return `What the fuck is ${aOrAn} \"${invalidInput}\"th place?`;
+  } else if (input < 0) {
+    return `What is this shit? We're not calculating negafibonacci here, man.`;
   }
 }
 
 const getFib = (place) => {
-  if (invalidInput(place)) {
-    return invalidInput(place)
-  }
-  place -= 1
-  let knownFibs = [0, 1]
-  if (place < 2) {return knownFibs[place];}
-  for (let i = 1; i < place; i++) {
+  const error = validateInput(place)
+  if (error) { return error; }
+
+  const fixedPlace = BigInt(place) - 1n
+  let knownFibs = [0n, 1n]
+  if (fixedPlace < 1) { return formatResult(place, knownFibs[fixedPlace]); }
+  for (let i = 1; i < fixedPlace; i++) {
     let first = knownFibs.shift()
     let second = knownFibs[0]
     knownFibs.push(first + second)
   }
-  return knownFibs.pop();
+  return formatResult(place, knownFibs.pop());
 }
