7eab570c024c9194550688658fa12f9d1e096d30
Unknown <perryx@gmail.com>
2020-03-02 19:58:31 -0700

    first api

---
+++ 2-piglatin/PigLatinApi/PigLatinApi/Controllers/TranslatorController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PigLatinApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TranslatorController : ControllerBase
+    {
+        // Hello, world!
+        // -> Ellohay, orldway!
+
+        [HttpGet]
+        public ActionResult<string> GetTranslation(string english)
+        {
+            return english;
+        }
+    }
+}
