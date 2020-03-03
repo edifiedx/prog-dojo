2c4f5d137e49c865a5000aa6eb68e427c417f8e4
Unknown <perryx@gmail.com>
2020-03-02 20:32:43 -0700

    translator first draft

--- 2-piglatin/PigLatinApi/PigLatinApi/Controllers/TranslatorController.cs
+++ 2-piglatin/PigLatinApi/PigLatinApi/Controllers/TranslatorController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace PigLatinApi.Controllers
@@ -16,7 +17,24 @@ namespace PigLatinApi.Controllers
         [HttpGet]
         public ActionResult<string> GetTranslation(string english)
         {
-            return english;
+            var result = new StringBuilder();
+            foreach(string word in english.Split(' '))
+            {
+                result.Append(TranslateWord(word)).Append(' ');
+            }
+
+            return result.ToString();
+        }
+
+        private string TranslateWord(string word)
+        {
+            var vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+            var firstVowelIndex = word.IndexOfAny(vowels);
+            var prefix = word[firstVowelIndex..];
+            var root = word[..firstVowelIndex];
+            var suffix = firstVowelIndex == 0 ? "way" : "ay"; 
+
+            return $"{prefix}{root}{suffix}";
         }
     }
 }
