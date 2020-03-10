23bbeb33fda5e658c8c31c2fd721cc9ed0634907
Unknown <perryx@gmail.com>
2020-03-10 11:11:19 -0600

    parameterized

--- 3-knapsack/knapsack.js
+++ 3-knapsack/knapsack.js
@@ -1,15 +1,14 @@
-const knapsack = () =>
+// weight, value
+
+const knapsack = (max, items) =>
 {
-    let max = 8
-    // weight, value
-    let items = [[1,2],[2,3],[5,4],[6,5]];
+    let best = 0;
     
     for (var i = 0; i < Math.pow(2, items.length); i++)
     {
         let bitwise = i.toString(2).padStart(4,0);
         let value = 0;
         let weight = 0;
-        let best = 0;
         for (var c = 0; c < bitwise.length; c++)
         {
             if (bitwise[c] == 1)
@@ -24,7 +23,7 @@ const knapsack = () =>
             // console.log(`${bitwise} : ${value}, ${weight}`);
         }
     }
-    return max;
+    return best;
 }
 
-console.log(knapsack());
+console.log(knapsack(8, [[1,2],[2,3],[5,4],[6,5]]));
